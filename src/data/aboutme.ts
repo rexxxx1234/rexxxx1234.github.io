6e617097c29ce3774315655c915ac964544b7180
Rex Ma <rexma1102@gmail.com>
2025-02-09 23:26:54 -0500

    update intro

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,19 +18,19 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
+  name: "Shihao(Rex) Ma",
   title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  institution: "University of Toronto",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "Rex is a PhD candidate in Computer Science at the University of Toronto, where he also received his BASc in Computer Engineering in 2020. He is passionate about applying machine learning and artificial intelligence to solve challenging problems in healthcare and biology.",
+  email: "rex.ma@mail.utoronto.ca",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
+  googleScholarUrl: "https://scholar.google.com/citations?user=JeW_QPYAAAAJ&hl=en",
+  githubUsername: "rexxxx1234",
+  linkedinUsername: "rex-ma-20a455113",
+  twitterUsername: "RexMa9",
   blogUrl: "https://",
   cvUrl: "https://",
   institutionUrl: "https://www.stanford.edu",
